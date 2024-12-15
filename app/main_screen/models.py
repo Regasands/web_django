edda226eb741b25b43fb31a9364b701e9ca3673e
Artemii <cvindergocvin@gmail.com>
2024-12-15 23:51:23 +0300

    create_head

--- app/main_screen/models.py
+++ app/main_screen/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
 # Create your models here.
+
+
+class ListNewsModel(models.Model):
+    name = models.CharField(verbose_name='Заголовок', max_length=255)
+
+    class Meta:
+        verbose_name = 'Данные Новости'
+        verbose_name_plural = 'Данные Новостей'
--- app/main_screen/views.py
+++ app/main_screen/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+
+from app.main_screen.models import ListNewsModel
+# Create your views here.
+
+
+class HomeScreenView(ListView):
+    model = ListNewsModel
 
 # Create your views here.
