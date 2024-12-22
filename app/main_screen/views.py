34f300dee4030b1117560b7a173dafff2e225bd0
Artemii <cvindergocvin@gmail.com>
2024-12-22 09:07:04 +0300

    upd;;

--- app/main_screen/views.py
+++ app/main_screen/views.py
@@ -33,6 +33,7 @@ class CreateUsersView(CreateView):
             group.user_set.add(user)
         return super().form_valid(form)
 
+
 class CreatePollViews(PermissionRequiredMixin, CreateView):
     form_class = PollCreateForm
     success_url = reverse_lazy('home_page')
@@ -59,5 +60,12 @@ class CheckPollView(PermissionRequiredMixin, ListView):
     def get_queryset(self):
         index_poll = self.kwargs.get('id_theam')
         obj = self.request.user.custom_user.poll_status
-        print(obj)
-        return super().get_queryset().exclude(pk__in=obj)
+        query = self.model.objects.filter(topic__pk=index_poll, status=True).exclude(pk__in=obj) if index_poll else []
+        return query
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['first_poll'] = self.get_queryset().first() 
+        print(context['first_poll']) # Передача первого элемента в контекст
+        return context
+
